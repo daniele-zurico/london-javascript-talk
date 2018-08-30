3b19db8b01d0e47ecf88d1c349cff383b689a1f7
daniele.zurico <daniele.zurico@gohenry.co.uk>
2018-08-30 12:05:41 +0100

    add hello world

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,37 @@
-let square = (value: number): number => {
-    return value * value;
-}
+import { ApolloServer } from "apollo-server";
+import { mergeSchemas } from "graphql-tools";
+import { GraphQLSchema } from "graphql";
+import {
+	gql,
+	makeExecutableSchema
+} from "apollo-server";
 
-console.log(square(4));
+
+const helloSchema: GraphQLSchema = makeExecutableSchema({
+	typeDefs: gql`
+		type Query {
+			hello: String
+		}
+	`
+});
+const helloResolver = {
+	Query: {
+		hello: () => {
+			return "Hello world!";
+		}
+	}
+};
+
+const schemas = [helloSchema];
+const resolvers = [helloResolver];
+
+const schema: GraphQLSchema = mergeSchemas({
+	schemas,
+	resolvers
+});
+
+const server = new ApolloServer({schema});
+
+server.listen().then(({ url }) => {
+	console.log(`🚀 Server ready at ${url}`);
+});
